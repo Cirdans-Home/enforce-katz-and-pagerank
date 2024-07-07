617792f796c609ea1fa0a9e0d17e8f6b758fa422
Cirdans-Home <f.durastante@na.iac.cnr.it>
2024-07-07 17:21:49 +0200

    Added Kendal Tau Check

--- enforcers/heatmaptest.m
+++ enforcers/heatmaptest.m
@@ -8,13 +8,15 @@ fprintf("Test matrices are: \n")
 fprintf("\t%s\n",d.name)
 
 perc = [0.1,0.2,0.3,0.4,0.5];
+betav = [1,0.8,0.6,0.4,0.2];
 
-Pert = NaN(length(d),length(perc)); % Value of the Objective
-Time = NaN(length(d),length(perc)); % Time To Solution
-Iter = NaN(length(d),length(perc)); % IPM Iter to Solution
-NNZ  = NaN(length(d),length(perc)); % Number of nonzeros in Delta
-NNZp = NaN(length(d),length(perc)); % Number of pos nonzeros in Delta
-NNZn = NaN(length(d),length(perc)); % Number of neg nonzeros in Delta
+Pert = NaN(length(d),length(perc),length(betav)); % Value of the Objective
+Time = NaN(length(d),length(perc),length(betav)); % Time To Solution
+Iter = NaN(length(d),length(perc),length(betav)); % IPM Iter to Solution
+NNZ  = NaN(length(d),length(perc),length(betav)); % Number of nonzeros in Delta
+NNZp = NaN(length(d),length(perc),length(betav)); % Number of pos nonzeros in Delta
+NNZn = NaN(length(d),length(perc),length(betav)); % Number of neg nonzeros in Delta
+KTAU = NaN(length(d),length(perc),length(betav)); % Number of neg nonzeros in Delta
 
 for i=1:length(d)
     load(fullfile(QP_problems_path,d(i).name));
@@ -31,41 +33,28 @@ for i=1:length(d)
     mu      = (I - alpha*A)\e;
     [~,bestmu] = sort(mu,"descend");
 
-    for j = 1:length(perc)
-        muhat = mu;
-        % Compute the changed mu
-        nindex  = round(perc(j)*n);
-        muhat(bestmu(1:nindex)) = mean(mu(bestmu(1:nindex)));
-        beta = 1;
-        [Delta,stat,mucheck] = enforce_katz(A,alpha,muhat,P,beta,1e-12);
+    for k = 1:length(betav)
+        for j = 1:length(perc)
+            muhat = mu;
+            % Compute the changed mu
+            nindex  = round(perc(j)*n);
+            muhat(bestmu(1:nindex)) = mean(mu(bestmu(1:nindex)));
+            beta = betav(k);
+            [Delta,stat,mucheck] = enforce_katz(A,alpha,muhat,P,beta,1e-12);
 
-        % Collect Data
-        % Value of the Objective
-        Pert(i,j) = beta*norm(Delta,"fro")^2 + (1-beta)*norm(Delta,1); 
-        Time(i,j) = stat.time; % Time To Solution
-        Iter(i,j) = stat.IPMiter; % IPM Iter to Solution
-        NNZ(i,j)  = nnz(Delta); % Number of nonzeros in Delta
-        NNZp(i,j) = nnz(Delta > 0); % Number of pos nonzeros in Delta
-        NNZn(i,j) = nnz(Delta < 0); % Number of neg nonzeros in Delta
+            % Collect Data
+            % Value of the Objective
+            Pert(i,j,k) = beta*norm(Delta,"fro")^2 + (1-beta)*norm(Delta,1);
+            Time(i,j,k) = stat.time; % Time To Solution
+            Iter(i,j,k) = stat.IPMiter; % IPM Iter to Solution
+            NNZ(i,j,k)  = nnz(Delta); % Number of nonzeros in Delta
+            NNZp(i,j,k) = nnz(Delta > 0); % Number of pos nonzeros in Delta
+            NNZn(i,j,k) = nnz(Delta < 0); % Number of neg nonzeros in Delta
+            KTAU(i,j,k) = corr(mucheck,muhat,'type','Kendall');
+        end
     end
 
 end
 
-%% Visualize Analysis
-try 
-    figure(1)
-    subplot(1,6,1)
-    heatmap(Pert)
-    subplot(1,6,2)
-    heatmap(Time)
-    subplot(1,6,3)
-    heatmap(Iter)
-    subplot(1,6,4)
-    heatmap(NNZ)
-    subplot(1,6,5)
-    heatmap(NNZp)
-    subplot(1,6,6)
-    heatmap(NNZn)
-catch
-    save('heatmapresults.mat',Pert,Time,Iter,NNZ,NNZp,NNZn)
-end
+%% Save Results
+save('heatmapresults.mat',Pert,Time,Iter,NNZ,NNZp,NNZn,KTAU)
