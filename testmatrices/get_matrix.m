f4dff28f6eec0c629e020c81ab86492605eba6f4
stefanocipolla <stefano.cipolla87@.com>
2024-07-27 22:54:23 +0200

    Added Dataset

--- testmatrices/get_matrix.m
+++ testmatrices/get_matrix.m
@@ -2,8 +2,12 @@ clear all
 close all 
 clc
 
-pb_class    = {'vanHeukelum/','AG-Monien/'};
-pb_name   = {'cage10','se'};
+pb_class    = {'JGD_SPG/','Arenas/','vanHeukelum/','vanHeukelum/','DIMACS10/',...
+                      'DIMACS10/', 'DIMACS10/','DIMACS10/','DIMACS10/','DIMACS10/',...
+                      'DIMACS10/','HB/','DIMACS10/','DIMACS10/','DIMACS10/'};
+pb_name     = {'EX5','PGPgiantcompo','cage10','cage11','cs4','ct2010','cti','data',...
+                        'de2010','delaunay_n16','fe_4elt2','gre_1107','nh2010','uk','vt2010'};
+
 
 for k = 1:length(pb_name)  
 Problem     = ssget([pb_class{k},pb_name{k}]);
